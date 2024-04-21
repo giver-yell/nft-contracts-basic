da3d9fd6d6cf0af3486e2f86fe92f9e7d392b013
giver-yell <giver.yell@gmail.com>
2024-04-22 08:32:50 +0900

    contract

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+
+contract MyNFT is ERC721 {
+
+    constructor() ERC721("MyNFT", "MyNFT") {}
+
+    function nftMint() public {}
+}
