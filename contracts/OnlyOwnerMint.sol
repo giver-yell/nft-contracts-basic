ba98e61c8480eeb1b56b37104f7ec32998f7dedb
giver-yell <giver.yell@gmail.com>
2024-04-25 07:43:56 +0900

    _msgSender

--- contracts/OnlyOwnerMint.sol
+++ contracts/OnlyOwnerMint.sol
@@ -6,7 +6,11 @@ import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 
 contract OnlyOwnerMint is ERC721 {
 
-    constructor() ERC721("OnlyOwnerMint", "OWNER") {}
+    address public owner;
+
+    constructor() ERC721("OnlyOwnerMint", "OWNER") {
+        owner =_msgSender();
+    }
 
     function nftMint(address to, uint256 tokenId) public {
         _mint(to, tokenId);
