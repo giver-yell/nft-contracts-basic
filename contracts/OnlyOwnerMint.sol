56652632b07764e61cc772d93741c4755609d059
giver-yell <giver.yell@gmail.com>
2024-04-25 07:51:16 +0900

    require

--- contracts/OnlyOwnerMint.sol
+++ contracts/OnlyOwnerMint.sol
@@ -6,13 +6,22 @@ import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 
 contract OnlyOwnerMint is ERC721 {
 
+    /**
+     * @dev
+     * - このコントラクトをデプロイしたアドレス用変数owner
+     */
     address public owner;
 
     constructor() ERC721("OnlyOwnerMint", "OWNER") {
         owner =_msgSender();
     }
 
-    function nftMint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
+    /**
+     * - このコントラクトをデプロイしたアドレスだけがmint可能 require
+     * - nftMint関数の実行アドレスにtokenIdを紐付け
+     */
+    function nftMint(uint256 tokenId) public {
+        require(owner == _msgSender(), "Caller is not the owner.");
+        _mint(_msgSender(), tokenId);
     }
 }
